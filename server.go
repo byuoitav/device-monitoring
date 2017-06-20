401bcdda85c740c7c9904e00bd1a444f107a6b0a
Clark Green <clark_green@byu.edu>
2017-06-20 17:04:11 -0600

    wholly cow

--- server.go
+++ server.go
@@ -3,18 +3,67 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/device-monitoring-microservice/device"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	devices, err := device.GetAddresses("ITB", "1101")
+
+	//get building and room info
+	hostname := os.Getenv("PI_HOSTNAME")
+	building := strings.Split(hostname, "-")[0]
+	room := strings.Split(hostname, "-")[1]
+
+	//get addresses from database
+	devices, err := device.GetAddresses(building, room)
 	if err != nil {
 		log.Printf("Houston, we have a problem.")
 	}
 
-	device.PingAddresses("ITB", "1101", devices)
+	//figure out how often to ping devices and start process in new goroutine
+	pingInterval := os.Getenv("DEVICE_PING_INTERVAL")
+	interval, err := strconv.Atoi(pingInterval)
+	if err != nil {
+
+		log.Printf("Error reading check interval. Terminating...")
+		os.Exit(1)
+
+	} else {
+
+		go func() {
+
+			for {
+
+				device.PingAddresses(building, room, devices)
+				time.Sleep(time.Duration(interval) * time.Second)
+
+			}
+
+		}()
+	}
+
+	port := ":10000"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
+	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	secure.GET("/health", Health)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	router.StartServer(&server)
 
 }
 
