1b8fa3f67f723b02c42fcc909195bd31eea25d4f
dannyrandall <dgrandall@me.com>
2017-08-08 12:34:07 -0600

    added version code

--- microservicestatus/microservicestatus.go
+++ microservicestatus/microservicestatus.go
@@ -1,5 +1,10 @@
 package microservicestatus
 
+import (
+	"bufio"
+	"os"
+)
+
 const (
 	StatusOK   = 0
 	StatusSick = 1
@@ -10,4 +15,22 @@ type Status struct {
 	Version    string `json:"version"`
 	Status     int    `json:"statuscode"`
 	StatusInfo string `json:"statusinfo"`
+}
+
+func GetVersion(fileLocation string) (string, error) {
+	file, err := os.Open(fileLocation)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // only read first line
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	version := scanner.Text()
+
+	return version, nil
 }
