1145d33bdf65043b14a9bfef0c62f6d7c77900d5
Daniel Randall <dgrandall@me.com>
2018-10-09 16:00:33 -0600

    try this script?

--- setenv.sh
+++ setenv.sh
@@ -3,18 +3,22 @@
 echo "branch " ${CIRCLE_BRANCH:=$(git rev-parse --abbrev-ref HEAD)}
 
 if [ "master" == "$CIRCLE_BRANCH" ]; then
+    echo "setting db vars to DEV"
     export DB_ADDRESS=DB_ADDRESS_DEV
     export DB_USERNAME=DB_USERNAME_DEV
     export DB_PASSWORD=DB_PASSWORD_DEV
 elif [ "testing" == "$CIRCLE_BRANCH" ]; then
+    echo "setting db vars to STG"
     export DB_ADDRESS=DB_ADDRESS_STG
     export DB_USERNAME=DB_USERNAME_STG
     export DB_PASSWORD=DB_PASSWORD_STG
 elif [ "stage" == "$CIRCLE_BRANCH" ]; then
+    echo "setting db vars to STG"
     export DB_ADDRESS=DB_ADDRESS_STG
     export DB_USERNAME=DB_USERNAME_STG
     export DB_PASSWORD=DB_PASSWORD_STG
 elif [ "production" == "$CIRCLE_BRANCH" ]; then
+    echo "setting db vars to PRD"
     export DB_ADDRESS=DB_ADDRESS
     export DB_USERNAME=DB_USERNAME
     export DB_PASSWORD=DB_PASSWORD
