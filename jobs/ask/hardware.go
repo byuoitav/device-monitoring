6bfb656c5d6c7c3ccda23792fafe28f3acb7b16d
Daniel Randall <dgrandall@me.com>
2018-12-03 15:41:43 -0700

    send events about hardware usage, collect average number of uninterruptible procs

--- jobs/ask/hardware.go
+++ jobs/ask/hardware.go
@@ -3,24 +3,29 @@ package ask
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/common/nerr"
 	"github.com/byuoitav/common/v2/events"
+	"github.com/byuoitav/device-monitoring/localsystem"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/docker"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/net"
+	"github.com/shirou/gopsutil/process"
 )
 
 const (
 	temperatureRootPath = "/sys/class/thermal"
+	uSleepCheckInterval = 3 * time.Second
 )
 
 // HardwareInfoJob gets hardware information about the device and pushes events up about it
@@ -29,15 +34,56 @@ type HardwareInfoJob struct{}
 // HardwareInfo is the struct of hardware information that is returned by this job.
 type HardwareInfo struct {
 	Host    map[string]interface{} `json:"host,omitempty"`
-	Memory  map[string]interface{} `json:"memory,omitemtpy"`
+	Memory  map[string]interface{} `json:"memory,omitempty"`
 	CPU     map[string]interface{} `json:"cpu,omitempty"`
 	Disk    map[string]interface{} `json:"disk,omitempty"`
 	Network map[string]interface{} `json:"network,omitempty"`
 	Docker  map[string]interface{} `json:"docker,omitempty"`
+	Procs   map[string]interface{} `json:"procs,omitempty"`
+}
+
+var (
+	avgProcsInUSleep float64
+)
+
+func init() {
+	// constantly measure the number of processes that are in sleep and get an average
+	avgProcsInUSleep = 0
+
+	ticker := time.NewTicker(uSleepCheckInterval)
+
+	go func() {
+		defer ticker.Stop()
+
+		for range ticker.C {
+			procs, err := process.Processes()
+			if err != nil {
+				log.L.Warnf("failed to get running processes: %s", err)
+				continue
+			}
+
+			count := 0
+
+			for _, p := range procs {
+				status, err := p.Status()
+				if err != nil {
+					continue
+				}
+
+				if status == "D" {
+					count++
+				}
+			}
+
+			avgProcsInUSleep = (avgProcsInUSleep + float64(count)) / 2
+			avgProcsInUSleep = round(avgProcsInUSleep, .05)
+		}
+	}()
 }
 
 // Run runs the job
 func (j *HardwareInfoJob) Run(ctx interface{}, eventWrite chan events.Event) interface{} {
+	log.SetLevel("debug")
 	ret := HardwareInfo{}
 	err := &nerr.E{}
 
@@ -73,6 +119,87 @@ func (j *HardwareInfoJob) Run(ctx interface{}, eventWrite chan events.Event) int
 		return nerr.Translate(err).Addf("failed to get hardware info")
 	}
 
+	ret.Procs, err = getProcsInfo()
+	if err != nil {
+		return nerr.Translate(err).Addf("failed to get hardware info")
+	}
+
+	systemID, err := localsystem.SystemID()
+	if err != nil {
+		log.L.Warnf("SYSTEM_ID not set, so I wont send any hardware events.")
+		return ret
+	}
+
+	// send event with all the information
+	event := events.Event{
+		GeneratingSystem: systemID,
+		Timestamp:        time.Now(),
+		EventTags:        []string{},
+		TargetDevice:     events.GenerateBasicDeviceInfo(systemID),
+		AffectedRoom:     events.GenerateBasicRoomInfo(systemID),
+		Key:              "hardware-info",
+		Data:             ret,
+	}
+	eventWrite <- event
+	event.Data = nil
+
+	// send info about cpu usage
+	if usage, ok := ret.CPU["usage"].(map[string]float64); ok {
+		if avg, ok := usage["avg"]; ok {
+			event.Key = "cpu-usage-percent"
+			event.Value = fmt.Sprintf("%v", avg)
+			eventWrite <- event
+		}
+	}
+
+	// send info about memory usage
+	if vMem, ok := ret.Memory["virtual"].(*mem.VirtualMemoryStat); ok {
+		event.Key = "v-mem-used-percent"
+		event.Value = fmt.Sprintf("%v", vMem.UsedPercent)
+		eventWrite <- event
+	}
+
+	// send info about swap usage
+	if sMem, ok := ret.Memory["swap"].(*mem.SwapMemoryStat); ok {
+		event.Key = "s-mem-used-percent"
+		event.Value = fmt.Sprintf("%v", sMem.UsedPercent)
+		eventWrite <- event
+	}
+
+	// send info about chip temp
+	if temps, ok := ret.Host["temperature"].(map[string]float64); ok {
+		for chip, temp := range temps {
+			event.Key = fmt.Sprintf("%s-temp", chip)
+			event.Value = fmt.Sprintf("%v", temp)
+			eventWrite <- event
+		}
+	}
+
+	// send info about # of writes
+	if counters, ok := ret.Disk["io-counters"]; ok {
+		if disks, ok := counters.(map[string]disk.IOCountersStat); ok {
+			for disk, stats := range disks {
+				event.Key = fmt.Sprintf("writes-to-%s", disk)
+				event.Value = fmt.Sprintf("%v", stats.WriteCount)
+				eventWrite <- event
+			}
+		}
+	}
+
+	// send info about total disk usage
+	if usage, ok := ret.Disk["usage"].(*disk.UsageStat); ok {
+		event.Key = "disk-used-percent"
+		event.Value = fmt.Sprintf("%v", usage.UsedPercent)
+		eventWrite <- event
+	}
+
+	// send info about avg # of processes in uninterruptible sleep
+	if avg, ok := ret.Procs["avg-procs-u-sleep"]; ok {
+		event.Key = "avg-procs-u-sleep"
+		event.Value = fmt.Sprintf("%v", avg)
+		eventWrite <- event
+	}
+
 	return ret
 }
 
@@ -97,7 +224,7 @@ func getCPUInfo() (map[string]interface{}, *nerr.E) {
 	}
 
 	for i := range percentages {
-		usage[fmt.Sprintf("cpu%d", i)] = percentages[i]
+		usage[fmt.Sprintf("cpu%d", i)] = round(percentages[i], .01)
 	}
 
 	// get average usage
@@ -107,7 +234,7 @@ func getCPUInfo() (map[string]interface{}, *nerr.E) {
 	}
 
 	if len(avgPercent) == 1 {
-		usage["avg"] = avgPercent[0]
+		usage["avg"] = round(avgPercent[0], .01)
 	}
 
 	return info, nil
@@ -121,6 +248,7 @@ func getMemoryInfo() (map[string]interface{}, *nerr.E) {
 		return info, nerr.Translate(err).Addf("failed to get memory info")
 	}
 
+	vMem.UsedPercent = round(vMem.UsedPercent, .01)
 	info["virtual"] = vMem
 
 	sMem, err := mem.SwapMemory()
@@ -128,6 +256,7 @@ func getMemoryInfo() (map[string]interface{}, *nerr.E) {
 		return info, nerr.Translate(err).Addf("failed to get memory info")
 	}
 
+	sMem.UsedPercent = round(sMem.UsedPercent, .01)
 	info["swap"] = sMem
 
 	return info, nil
@@ -150,7 +279,7 @@ func getHostInfo() (map[string]interface{}, *nerr.E) {
 
 	info["users"] = users
 
-	temps := make(map[string]interface{})
+	temps := make(map[string]float64)
 	count := make(map[string]int)
 	info["temperature"] = temps
 
@@ -193,6 +322,7 @@ func getDiskInfo() (map[string]interface{}, *nerr.E) {
 		return info, nerr.Translate(err).Addf("failed to get host info")
 	}
 
+	usage.UsedPercent = round(usage.UsedPercent, .01)
 	info["usage"] = usage
 
 	ioCounters, err := disk.IOCounters("sda", "mmcblk0")
@@ -229,4 +359,40 @@ func getDockerInfo() (map[string]interface{}, *nerr.E) {
 	info["stats"] = stats
 
 	return info, nil
+}
+
+func getProcsInfo() (map[string]interface{}, *nerr.E) {
+	info := make(map[string]interface{})
+
+	procs, err := process.Processes()
+	if err != nil {
+		return info, nerr.Translate(err).Addf("failed to get processes info")
+	}
+
+	bad := []string{}
+
+	for _, p := range procs {
+		status, err := p.Status()
+		if err != nil {
+			continue
+		}
+
+		if status == "D" {
+			name, err := p.Name()
+			if err != nil {
+				name = fmt.Sprintf("unable to get name: %s", name)
+			}
+
+			bad = append(bad, name)
+		}
+	}
+
+	info["cur-procs-u-sleep"] = bad
+	info["avg-procs-u-sleep"] = avgProcsInUSleep
+
+	return info, nil
+}
+
+func round(x, unit float64) float64 {
+	return math.Round(x/unit) * unit
 }
