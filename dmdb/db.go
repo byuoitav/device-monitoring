58c7e34ab258e374454909c9d9b3e474cf1d7a8d
Daniel Randall <dgrandall@me.com>
2018-12-04 12:16:28 -0700

    badger db for dmm, maintenance mode uses that and gob for persistant storage

---
+++ dmdb/db.go
@@ -0,0 +1,90 @@
+package dmdb
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/nerr"
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbLocation = "db"
+)
+
+var (
+	once sync.Once
+	db   *badger.DB
+)
+
+func initDB() {
+	once.Do(func() {
+		opts := badger.DefaultOptions
+		opts.Dir = dbLocation
+		opts.ValueDir = dbLocation
+
+		var err error
+		log.L.Infof("Opening local database at: %s", dbLocation)
+
+		db, err = badger.Open(opts)
+		if err != nil {
+			log.L.Fatalf("failed to open badger db: %s", err)
+		}
+
+		// close the database connection if the program is killed
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			log.L.Infof("Closing local database.")
+			err := db.Close()
+			if err != nil {
+				log.L.Warnf("failed to close local database: %s", err)
+			}
+
+			os.Exit(0)
+		}()
+	})
+}
+
+// Put puts a key/value into the database
+func Put(key string, value []byte) *nerr.E {
+	initDB()
+
+	err := db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(key), value)
+	})
+	if err != nil {
+		return nerr.Translate(err).Addf("failed to put key %s into the database", key)
+	}
+
+	return nil
+}
+
+// Get returns a value at the given key
+func Get(key string) ([]byte, *nerr.E) {
+	initDB()
+	value := []byte{}
+
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		value, err = item.ValueCopy(nil)
+		return err
+	})
+	if err != nil {
+		if err == badger.ErrKeyNotFound {
+			return value, nil
+		}
+
+		return value, nerr.Translate(err).Addf("failed to get '%s' from the local database", key)
+	}
+
+	return value, nil
+}
--- handlers/maintmode.go
+++ handlers/maintmode.go
@@ -1,28 +1,58 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/nerr"
 	"github.com/byuoitav/common/v2/events"
+	"github.com/byuoitav/device-monitoring/dmdb"
 	"github.com/byuoitav/device-monitoring/jobs"
 	"github.com/byuoitav/device-monitoring/localsystem"
 	"github.com/labstack/echo"
 )
 
-var inMaintMode bool
+const (
+	maintKey = "maintenance"
+)
+
+func init() {
+	val, err := isInMaintMode()
+	if err != nil {
+		log.L.Fatalf("failed to get initial maintenance mode value: %s", err)
+	}
+
+	log.L.Infof("Maintenance mode is set to %v", val)
+}
 
 // IsInMaintMode returns whether or not the ui is in maintenance mode
 func IsInMaintMode(ctx echo.Context) error {
-	return ctx.String(http.StatusOK, fmt.Sprintf("%v", inMaintMode))
+	val, err := isInMaintMode()
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("%v", err.Error()))
+	}
+
+	return ctx.String(http.StatusOK, fmt.Sprintf("%v", val))
 }
 
 // ToggleMaintMode swaps test mode to active/inactive
 func ToggleMaintMode(ctx echo.Context) error {
-	inMaintMode = !inMaintMode
-	log.L.Infof("Setting maintenance mode to %v", inMaintMode)
+	val, err := isInMaintMode()
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("%v", err.Error()))
+	}
+
+	val = !val
+	log.L.Infof("Setting maintenance mode to %v", val)
+
+	err = setMaintMode(val)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("%v", err.Error()))
+	}
 
 	jobs.Messenger().SendEvent(events.Event{
 		GeneratingSystem: localsystem.MustSystemID(),
@@ -31,8 +61,52 @@ func ToggleMaintMode(ctx echo.Context) error {
 		TargetDevice:     events.GenerateBasicDeviceInfo(localsystem.MustSystemID()),
 		AffectedRoom:     events.GenerateBasicRoomInfo(localsystem.MustRoomID()),
 		Key:              "in-maintenance-mode",
-		Value:            fmt.Sprintf("%v", inMaintMode),
+		Value:            fmt.Sprintf("%v", val),
 	})
 
-	return ctx.String(http.StatusOK, fmt.Sprintf("%v", inMaintMode))
+	return ctx.String(http.StatusOK, fmt.Sprintf("%v", val))
+}
+
+func isInMaintMode() (bool, *nerr.E) {
+	val := false
+
+	b, err := dmdb.Get(maintKey)
+	if err != nil {
+		return val, err.Addf("failed to get maintenance mode")
+	}
+
+	// it just hasn't been set yet, so we should set it to false
+	if len(b) == 0 {
+		err = setMaintMode(val)
+		if err != nil {
+			err.Addf("failed to get maintenance mode")
+		}
+
+		return val, err
+	}
+
+	dec := gob.NewDecoder(bytes.NewBuffer(b))
+	gerr := dec.Decode(&val)
+	if gerr != nil {
+		return val, nerr.Translate(gerr).Addf("failed to get maintenance mode")
+	}
+
+	return val, nil
+}
+
+func setMaintMode(val bool) *nerr.E {
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+
+	gerr := enc.Encode(val)
+	if gerr != nil {
+		return nerr.Translate(gerr).Addf("failed to set maintenance mode")
+	}
+
+	err := dmdb.Put(maintKey, buf.Bytes())
+	if err != nil {
+		return err.Addf("failed to set maintenance mode")
+	}
+
+	return nil
 }
