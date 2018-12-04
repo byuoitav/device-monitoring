cc3b1b391e13397d4ebc0718db4d91512660c0f0
Daniel Randall <dgrandall@me.com>
2018-12-04 14:14:27 -0700

    persistant wide sidenav

--- dashboard/src/app/components/app/app.component.ts
+++ dashboard/src/app/components/app/app.component.ts
@@ -8,12 +8,35 @@ import { APIService } from "../../services/api.service";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  public wideSidenav = true;
   public id: string;
+  private urlParams: URLSearchParams;
 
   constructor(public api: APIService) {}
 
   public async ngOnInit() {
+    this.urlParams = new URLSearchParams(window.location.search);
     this.id = await this.api.getDeviceID();
+  }
+
+  public wideSidenav(): boolean {
+    if (typeof this.urlParams === "undefined" || this.urlParams === null) {
+      return false;
+    }
+
+    if (this.urlParams.has("wide-sidenav")) {
+      return this.urlParams.get("wide-sidenav").toLowerCase() === "true";
+    }
+
+    return true;
+  }
+
+  public setWideSidenav(val: boolean) {
+    this.urlParams.set("wide-sidenav", val);
+
+    window.history.replaceState(
+      null,
+      "System Health Dashboard",
+      window.location.pathname + "?" + this.urlParams.toString()
+    );
   }
 }
