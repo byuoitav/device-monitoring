5385bce20db9d8af434582bd617b79a023737fc6
Andrew Werner <42447312+werndog@users.noreply.github.com>
2020-06-02 13:58:39 -0600

    fixing divide by zero error (#181)
    This closes #169.
    
    * fixing divide by zero error
    
    * adding in error check

--- actions/ping/internal.go
+++ actions/ping/internal.go
@@ -96,9 +96,15 @@ func (p *Pinger) ping(ctx context.Context, host *host, config Config) *Result {
 	}
 
 	// calculate info in result
-	avgrtt /= time.Duration(result.PacketsSent)
-	if avgrtt != 0 {
-		result.AverageRoundTrip = avgrtt.String()
+	if len(result.PacketsSent) != 0 {
+		avgrtt /= time.Duration(result.PacketsSent)
+		if avgrtt != 0 {
+			result.AverageRoundTrip = avgrtt.String()
+		}
+	} else {
+		if result.Error == "" {
+			result.Error = "no packets were sent"
+		}
 	}
 
 	return result
